a02654afd95520a93df016b3254d260fe082d0c7
Miguel Villarica <mamvillarica@gmail.com>
2022-01-29 00:40:18 +0800

    added the video and aspect ratio almost working

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector(".navbar")
 const button = document.querySelector(".nav-btn-icon")
 
+// opens the navbar and changes button icon
 function openNav() {
 	if (navbar.className === "navbar") {
 		navbar.className += " responsive"
